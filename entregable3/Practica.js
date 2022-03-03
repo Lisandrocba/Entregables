bbf5e38c64c336bd0ea37981575f030e76a049ca
lisandro cordoba <lisandrocba_7@hotmail.com>
2022-03-03 12:21:01 -0300

    cambios de la tercera entrega

--- entregable3/Practica.js
+++ entregable3/Practica.js
@@ -1,33 +1,40 @@
 const mostrarPalabras = (texto, cb, time = 1000) =>{
-    let i = 0;
-    let j = texto.split(" ");
-
-
-    const recorrerTexto =()=>{
-        if(i < j.length){
-            console.log(j[i]);
-            i++;
+    return new Promise((resolve) =>{
+        let i = 0;
+        let j = texto.split(" ");
+    
+    
+        const recorrerTexto =()=>{
+            if(i < j.length){
+                console.log(j[i]);
+                i++;
+            }
+            else{
+                clearInterval(timer);
+                cb();
+                resolve(i)
+            }
         }
-        else{
-            clearInterval(timer);
-            cb();
-            console.log('Cantidad de palabras: '+i)
-        }
-    }
-
-    const timer = setInterval(recorrerTexto, time)
+    
+        const timer = setInterval(recorrerTexto, time)
+    })
+   
 }
 
 const funcionFinalizado =()=> console.log('Proceso Completo')
 
-setTimeout(()=>{
-    mostrarPalabras('hola soy lisandro', funcionFinalizado)
-},0)
+const invocarFuncion = async() =>{ 
+    let tot = 0; 
+    tot += await mostrarPalabras("La tercera guerra mundial", funcionFinalizado, 300); 
+    tot += await mostrarPalabras("no puede suceder", funcionFinalizado, 300); 
+    tot += await mostrarPalabras("por el bien de la humanidad", funcionFinalizado, 300);
 
-setTimeout(()=>{
-    mostrarPalabras('vivo en la ciudad de cosquin', funcionFinalizado)
-},0)
+console.log(`proceso completo`) 
+console.log(`El total de palabres fue= ${tot}`)
+};
 
-setTimeout(()=>{
-    mostrarPalabras('hincha perro de river', funcionFinalizado)
-},0)
+invocarFuncion();
+
+
+
+
