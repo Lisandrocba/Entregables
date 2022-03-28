3ac32c6eba013c400ac5a867b040926023e1cb7e
lisandro cordoba <lisandrocba_7@hotmail.com>
2022-03-28 13:54:21 -0300

    cambio ejercicio numero random

--- entregable7/app.js
+++ entregable7/app.js
@@ -1,39 +1,48 @@
 import express from "express";
 import {Archivo} from '../entregable6/index.js'
 
+const archivo1 = new Archivo('productos.txt');
 const producto1 = { title: 'cafe', price: 300, thimbnail: 'url.foto'}
 const producto2 = { title: 'fideos', price: 180, thimbnail: 'url.foto2'}
+const producto3 = { title: 'cerveza', price: 380, thimbnail: 'url.foto3'}
+const producto4 = { title: 'mani', price: 80, thimbnail: 'url.foto4'}
 
-const archivo1 = new Archivo('productos.txt');
-archivo1.guardar(producto1)
+archivo1.guardar(producto1);
+archivo1.guardar(producto2);
+archivo1.guardar(producto3);
+archivo1.guardar(producto4);
 
-const articulo = archivo1.leer()
+const archivo = archivo1.leer()
 
 let contadorItem=0;
 let contadorItems=0;
-const items = [{item: articulo}]
+let items = {item: archivo}
+console.log('este',items)
+console.log(items.item.length)
 const contador = 0;
-const objetoProductos = {items: {nombre: 'arroz', precio: 120}, cantiad: {cantidad: 10}}
-
-
+const objetoProductos = [items= { ...items, cantidad: 50}]
 
 
 const app = express();
 const PORT = 8080;
 const server = app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`);
-    console.log(items)
+    console.log()
     
 })
 
+const numeroRandom = Math.random()*(items.item.length - 1)+1;
+const resultado = numeroRandom.toFixed(0);
+const itemRandom = items.item.filter(i => i.id === parseInt(resultado))
+console.log(resultado)
 app.get('/item', (req, res)=>{
     ++contadorItem
-    res.send(objetoProductos)
+    res.send(itemRandom)
  })
 
 app.get('/items', (req, res)=>{
     ++contadorItems
-   res.send(items)
+   res.send(JSON.parse(archivo1.leer()))
 }) 
 
 app.get('/visitas', (req, res)=>{
