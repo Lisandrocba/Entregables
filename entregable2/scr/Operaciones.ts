56adfb953c86542c9710c4719e3801e34e77dd17
lisandro cordoba <lisandrocba_7@hotmail.com>
2022-08-24 00:03:49 -0300

    cambios

--- entregable2/scr/Operaciones.ts
+++
@@ -1,7 +0,0 @@
-const operaciones = async(num1: number, num2: number, nombre: string) => {
-    let oper = await import('./Operacion.js')
-    let resultado = oper.operacion(num1,num2, nombre);
-    return console.log(resultado);
-}
-
-operaciones(2,3,'suma');
