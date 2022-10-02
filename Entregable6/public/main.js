914cd2b3ecab5da6e761e5e68a6e03b5318b9d41
lisandro cordoba <lisandrocba_7@hotmail.com>
2022-10-02 17:45:45 -0300

    entregable numero 6

---
+++ Entregable6/public/main.js
@@ -0,0 +1,55 @@
+let socket = io.connect('http://localhost:3000', {'forceNew' : true});
+
+
+socket.on('productos', (data)=>{
+    render(data)
+})
+
+socket.on('listaChat', data=>{
+    renderChat(data)
+})
+
+const render =(productos)=>{
+    const html = productos.map(prod=>{
+        return(`
+                <tr>
+                    <td>${prod.nombreProducto}</td>
+                    <td>${prod.precioProducto}</td>
+                    <td>${prod.stockProducto}</td>
+                </tr>
+        `)
+    }).join(' ')
+    document.getElementById('listaProductos').innerHTML = html
+}
+
+const renderChat =(chat)=>{
+    const html = chat.map(item=>{
+        return(`
+                <tr>
+                    <td>${item.email}</td>
+                    <td>${item.msj}</td>
+                    <td>${item.hora}</td>
+                </tr>
+        `)
+    }).join(' ')
+    document.getElementById('chat').innerHTML = html
+}
+
+const addProd =(e)=>{
+    let payload = {
+        nombreProducto : document.getElementById('nombreProducto').value,
+        precioProducto : document.getElementById('precioProducto').value,
+        stockProducto : document.getElementById('stockProducto').value
+    }
+
+    socket.emit('nuevoProducto', payload)
+}
+
+const addMsj =()=>{
+    let payload = {
+        email : document.getElementById('email').value,
+        msj: document.getElementById('msj').value,
+        hora: `${new Date().getHours()} : ${new Date().getMinutes()} : ${new Date().getSeconds()}`
+    }
+    socket.emit('chat', payload)
+}
---
+++ Entregable6/server/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.Server(app)
+const io = require('socket.io')(server)
+const PORT = 3000
+
+const productos=[{id:1, nombreProducto: 'fideo', precioProducto: 200, stockProducto: 1500}]
+const conversacion = []
+
+app.use(express.static('public'))
+
+app.get('/',(req, res)=>{
+    
+})
+
+//socket formulario
+io.on('connect',(socket)=>{
+    console.log('cliente conectado')
+    socket.emit('productos', productos)
+    socket.emit('listaChat', conversacion)
+    socket.on('nuevoProducto', data =>{
+        console.log('llego')
+        productos.push(data)
+        io.sockets.emit('productos', productos)
+    })
+    socket.on('chat', data=>{
+        conversacion.push(data)
+        io.sockets.emit('listaChat', conversacion)
+    })
+})
+
+
+
+
+
+server.listen(PORT, ()=>{
+    console.log(`servidor escuchando al puerto ${PORT}`)
+})
